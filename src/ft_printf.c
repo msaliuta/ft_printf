458532059f60f8b5d4c087f262ce62ab8f5600f5
Maksym SALIUTA <msaliuta@e1r2p1.unit.ua>
2019-07-07 08:20:17 +0300

    wed perty

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -3,24 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msaliuta <msaliuta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: msaliuta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 12:05:22 by msaliuta          #+#    #+#             */
-/*   Updated: 2019/07/03 07:08:39 by msaliuta         ###   ########.fr       */
+/*   Updated: 2019/07/06 19:18:11 by msaliuta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_printf(const char *restrict fmt, ...)
+int		ft_printf(const char *restrict frmt, ...)
 {
-	t_pf_env	e;
+	t_pf_env	o;
 
-	ft_bzero(&e, sizeof(e));
-	e.fd = 1;
-	va_start(e.ap[0], fmt);
-	va_copy(e.ap[1], e.ap[0]);
-	work_fpf(fmt, &e);
-	va_end(e.ap[0]);
-	return (e.ret);
+	ft_bzero(&o, sizeof(o));
+	o.fd = 1;
+	va_start(o.ap[0], frmt);
+	start_fpf(frmt, &o);
+	va_end(o.ap[0]);
+	return (o.ret);
+}
+
+void	start_fpf(const char *restrict frmt, t_pf_env *o)
+{
+	while (frmt[o->i])
+	{
+		if (IPRSNT(frmt[o->i]) && !frmt[o->i + 1])
+		{
+			o->ret = 0;
+			break ;
+		}
+		else if (IPRSNT(frmt[o->i]) && !IPRSNT(frmt[o->i + 1]))
+		{
+			++o->i;
+			get_tags(frmt, o);
+			get_flags(frmt, o);
+			get_specs(frmt, o);
+		}
+		else if (IPRSNT(frmt[o->i]) && IPRSNT(frmt[o->i + 1]))
+		{
+			o->ret += write(o->fd, "%", 1);
+			o->i += 2;
+		}
+		else
+			o->ret += write(o->fd, &frmt[o->i++], 1);
+	}
+}
+
+void	parse_flags(const char *restrict frmt, t_pf_env *o)
+{
+	while (ft_strchr(I_FLAG, frmt[o->i]))
+	{
+		frmt[o->i] == ' ' ? o->flag.sps = 1 : 0;
+		frmt[o->i] == '-' ? o->flag.minus = 1 : 0;
+		frmt[o->i] == '+' ? o->flag.plus = 1 : 0;
+		frmt[o->i] == '#' ? o->flag.hash = 1 : 0;
+		frmt[o->i] == '0' ? o->flag.zero = 1 : 0;
+		if (frmt[o->i] >= 'L' && frmt[o->i] <= 'z')
+			get_mods(frmt, o);
+		if (frmt[o->i] == '*')
+			get_width(o);
+		if (frmt[o->i] == '.')
+			get_prec(frmt, o);
+		else if (INUM1(frmt[o->i]) && o->flag.prec < 0)
+		{
+			o->flag.width = ft_atoi(frmt + o->i);
+			while (INUM(frmt[o->i]))
+				++o->i;
+		}
+		else
+			++o->i;
+	}
 }
--- src/work_fpf.c
+++ src/work_fpf.c
@@ -3,42 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   work_fpf.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msaliuta <msaliuta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: msaliuta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 07:06:53 by msaliuta          #+#    #+#             */
-/*   Updated: 2019/07/03 07:46:48 by msaliuta         ###   ########.fr       */
+/*   Updated: 2019/07/06 19:18:19 by msaliuta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	work_fpf(const char *restrict fmt, t_pf_env *e)
-{
-	while (fmt[e->i])
-	{
-		if (fmt[e->i] == '{')
-			check_set(fmt, e);
-		else if (IPRSNT(fmt[e->i]) && !fmt[e->i + 1])
-		{
-			e->ret = 0;
-			break ;
-		}
-		else if (IPRSNT(fmt[e->i]) && !IPRSNT(fmt[e->i + 1]))
-		{
-			++e->i;
-			get_tags(fmt, e);
-			get_flags(fmt, e);
-			get_specs(fmt, e);
-		}
-		else if (IPRSNT(fmt[e->i]) && IPRSNT(fmt[e->i + 1]))
-		{
-			e->ret += write(e->fd, "%", 1);
-			e->i += 2;
-		}
-		else
-			e->ret += write(e->fd, &fmt[e->i++], 1);
-	}
-}
 
 void	init_flags(t_pf_flag *flag)
 {
